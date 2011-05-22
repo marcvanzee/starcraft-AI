633ddca7fb33132837af6436defb88b5ba7f2ae2
fmeeuwen@gmail.com <fmeeuwen@gmail.com@fc26f891-f90e-dafd-f671-8ef1ba7eadf4>
2011-05-22 21:11:13 +0000

    Removed the Environment.java and made Env.java implement the BWAPIEventListener. It throws events for game connected and gamestarted.

--- trunk/BWAPI Environment/src/starcraft/Env.java
+++ trunk/BWAPI Environment/src/starcraft/Env.java
@@ -2,32 +2,63 @@ package starcraft;
 
 import java.util.logging.Logger;
 
+import eisbot.proxy.BWAPIEventListener;
 import eisbot.proxy.ExampleAIClient;
+import eisbot.proxy.JNIBWAPI;
 
+import apapl.data.APLFunction;
 import apapl.data.APLIdent;
 import apapl.data.APLList;
 import apapl.data.APLListVar;
 import apapl.data.APLNum;
 import apapl.data.Term;
 
-public class Env extends apapl.Environment
+public class Env extends apapl.Environment implements BWAPIEventListener
 {
 	
-	 private Logger _logger = Logger.getLogger("EMarket."+Env.class.getName());
-	 private Environment environment;
+	 private Logger _logger = Logger.getLogger("Starcraft."+Env.class.getName());
+	 
+	 private JNIBWAPI _jnibwapi;
+	 private Thread _clientThread;
+	 //The agent names.
+	 private String[] _agentNames = {"officer1"};
+	 
 	 
 	 public Env()
 	 {
-		 System.out.println("Loading Environment");
-		 environment = new Environment();
-		 System.out.println("Environment loaded");
+		init();
 	 }
+	 
+	 /**
+	  * Initializes the environment.
+	  */
+	 private void init()
+	 {
+		 _jnibwapi = new JNIBWAPI(this);
+		 _clientThread = new Thread(new JNIBWAPIClient(_jnibwapi));
+	 }
+	 
+	 /**
+	  * Starts the jnibwapi client thread.
+	  */
+	 private void start()
+	 {
+		 if(!_clientThread.isAlive())
+			 _clientThread.start();
+	 }
+	 	 
+	 
+	 public synchronized Term start(String agentName)
+	 {
+		start();
+		return wrapBoolean(true);
+	 }
+	 
 	 
 	 /* Test method for logging*/
 	 public synchronized Term log(String agentName,APLIdent var)
 	 {
-	    	_logger.info("Performing testAction1" + agentName);
-	    	
+	    	_logger.info(agentName + ":" + var.toString());
 	    	return wrapBoolean(true);
 	 }
 	    
@@ -41,14 +72,6 @@ public class Env extends apapl.Environment
 	    	}
 	    	return wrapBoolean(true);
 	  }
-	  
-	 /* Test method for starting ExampleAICLient */
-	 public synchronized Term start(String agentName)
-	 {
-		 System.out.println("going to perform test method");
-		 environment.testMethod();
-		 return wrapBoolean(true);
-	 }
 	 
 	 public static  APLListVar wrapBoolean( boolean b )
 	 {
@@ -57,7 +80,112 @@ public class Env extends apapl.Environment
 	 
 	 public static void main(String[] args)
 	 {
-		 new eisbot.proxy.ExampleAIClient();
+		new Env();
 	 }
+	 
+	
+	 public void throwEventToAll(String name)
+	 {
+		//Note the use of a function with 1 meaningless argument, since no arguments are not supported for throwing as event.
+		 APLFunction event = new APLFunction(name, new APLIdent("true"));
+		 super.throwEvent(event, _agentNames);
+	 }
+	 
+	///BWAPI Event Listener functions. 
+	@Override
+	public void connected() 
+	{
+		throwEventToAll("connected");
+	}
+
+	@Override
+	public void gameStarted() 
+	{
+		throwEventToAll("gameStarted");		
+	}
+
+	@Override
+	public void gameUpdate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void gameEnded() 
+	{
+		throwEventToAll("gameEnded");		
+	}
+
+	@Override
+	public void keyPressed(int keyCode) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void matchEnded(boolean winner) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void playerLeft(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void nukeDetect(int x, int y) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void nukeDetect() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void unitDiscover(int unitID) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void unitEvade(int unitID) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void unitShow(int unitID) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void unitHide(int unitID) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void unitCreate(int unitID) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void unitDestroy(int unitID) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void unitMorph(int unitID) {
+		// TODO Auto-generated method stub
+		
+	}
 	 
 }
